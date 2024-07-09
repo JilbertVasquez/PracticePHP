1ee4512c63ce3be822406a9752a7797af5774d67
JilbertVasquez <jilbertvasquez@gmail.com>
2024-07-09 23:00:03 +0800

    change section front

--- changepass.php
+++ changepass.php
@@ -1,3 +1,19 @@
-<section >
-    heeeeeeeeeeeeeeeeeeeeeeasdad
+
+
+<section class="changepass-sec container-fluid d-flex justify-content-center mt-10">
+    <div class="changepass-inner container-fluid h-full bg-black d-flex justify-content-center">
+        <div class="p-2 mt-3 mb-3 bg-primary w_4_5 ">
+            <div class="container-full d-flex justify-content-center align-items-center">
+                <h1>Change Password</h1>
+            </div>
+            
+            <form action="" method="POST" >
+                    <h1>New Password</h1>
+                    <input class="newpass p-2 rounded" type="text" name="newpwd" placeholder="Password...">
+                    <h1>Retype New Password</h1>
+                    <input class="renewpass p-2 rounded" type="text" name="newpwd" placeholder="Retype New Password...">
+            </form>
+            <button class="btn btn-primary submit_button">Submit</button>
+        </div>
+    </div>
 </section>
